ca3438c858e5a8c84244ef1164b1bc81c3296f20
Sathish Murugesan <Sathishkumar.Murugesan@cognizant.com>
2019-08-26 21:34:40 +0200

    Angular Front End Cognito quick start

---
+++ 06.aws-cognito-angular-quickstart/src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: true,
+
+    region: 'us-east-2',
+
+    identityPoolId: 'us-east-2:398fb652-3808-4952-9c1a-3e02a241b9bf',
+    userPoolId: 'us-east-2_GpXD8dZOJ',
+    clientId: '6sqbbu79qodvssl7fh02g70ka6',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-2',
+
+    ddbTableName: 'LoginTrailin28minutescognito',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+};
+
---
+++ 06.aws-cognito-angular-quickstart/src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: false,
+
+    region: 'us-east-2',
+
+    identityPoolId: 'us-east-2:f5b30d21-d4c2-4b43-956d-ecc7aa816533\n',
+    userPoolId: 'us-east-2_1fRzsFqSc',
+    clientId: '6b7ca88jda38d9i2cov71k9c2o',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-2',
+
+    ddbTableName: 'LoginTrailin28minutescognito',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+};
+
